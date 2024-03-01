345b948ac1c0692518a4db13083001f8e6cb292a
keyule <keyule@gmail.com>
2024-03-02 01:38:05 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { ThreeDCardDemo } from './components/3dCard'
+
+
+function App() {
+
+  return (
+      <div className="h-screen w-full bg-black">
+        <ThreeDCardDemo/> 
+      </div>
+  
+  )
+}
+
+export default App
