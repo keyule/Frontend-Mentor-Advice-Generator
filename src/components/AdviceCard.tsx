50dfd45dec0ecdf77fe3d777001320488383bc69
yule <keyule@gmail.com>
2024-03-03 19:16:07 +0800

    finished

--- src/components/AdviceCard.tsx
+++ src/components/AdviceCard.tsx
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import diceIcon from '../assets/images/icon-dice.svg'; // Ensure you have a dice icon in your src folder
+import diceIcon from '../assets/images/icon-dice.svg';
+import dividerDesktop from '../assets/images/pattern-divider-desktop.svg'; 
+import dividerMobile from '../assets/images/pattern-divider-mobile.svg'; 
+import { TextGenerateEffect } from "../components/ui/text-generate-effect";
 
 const AdviceCard: React.FC = () => {
   const [advice, setAdvice] = useState({ id: '71', advice: 'It is easy to sit up and take notice, what\'s difficult is getting up and taking action.' });
+  const [effectKey, setEffectKey] = useState(0);
 
   useEffect(() => {
     // Fetch the specific advice when the component mounts
@@ -14,6 +18,7 @@ const AdviceCard: React.FC = () => {
       const response = await fetch(`https://api.adviceslip.com/advice/${id}`);
       const data = await response.json();
       setAdvice({ id: data.slip.id, advice: data.slip.advice });
+      setEffectKey(prevKey => prevKey + 1); 
     } catch (error) {
       console.error('Error fetching advice:', error);
     }
@@ -25,24 +30,31 @@ const AdviceCard: React.FC = () => {
       const response = await fetch('https://api.adviceslip.com/advice', {cache: "no-cache" });
       const data = await response.json();
       setAdvice({ id: data.slip.id, advice: data.slip.advice });
+      setEffectKey(prevKey => prevKey + 1); 
     } catch (error) {
       console.error('Error fetching random advice:', error);
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-900">
-      <div className="bg-gray-800 p-10 rounded-lg shadow-xl max-w-xs sm:max-w-md">
-        <div className="mb-4">
-          <div className="text-gray-400 text-sm">ADVICE #{advice.id}</div>
+    <div className="flex items-center justify-center h-screen bg-dark-blue">
+      <div className="bg-dark-grayish-blue p-12 rounded-xl shadow-xl max-w-sm sm:max-w-lg">
+        <div className = "mb-6">
+          <div className="text-neon-green text-sm text-center font-Manrope tracking-5 font-bold">ADVICE #{advice.id}</div>
         </div>
-        <div className="text-white text-lg text-center mb-4">
-          "{advice.advice}"
+        <div className="text-light-cyan text-3xl text-center font-Manrope font-semi-bold">
+          <TextGenerateEffect key= {effectKey} words={`"${advice.advice}"`} />
         </div>
-        <div className="flex justify-center">
+        <div className="translate-y-12">
+          <picture>
+            <source srcSet={dividerDesktop} media="(min-width: 768px)" />
+            <img src={dividerMobile} alt="Divider" style={{ width: '100%', display: 'block' }} />
+          </picture>
+        </div>
+        <div className="flex justify-center translate-y-20">
           <button
             onClick={fetchRandomAdvice}
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full flex items-center justify-center w-12 h-12"
+            className="bg-neon-green hover:bg-green-400 py-2 px-4 rounded-full flex items-center justify-center w-16 h-16"
           >
             <img src={diceIcon} alt="Dice" className="" />
           </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    letterSpacing: {
+      '1': '0em',
+      '2': '0.025em',
+      '3': '0.05em',
+      '4': '0.1em',
+      '5': '0.2em',
+    },
     extend: {
       colors: {
         'light-cyan': '#cde2e8',
@@ -12,6 +19,27 @@ export default {
         'grayish-blue': '#4e5c73',
         'dark-grayish-blue': '#313a48',
         'dark-blue': '#1f2632',
+      },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+      },
+      translate: {
+        '22': '5.5rem',
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
       },
     },
   },
